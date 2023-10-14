1ea21080190a31ca6bb3b4a21c5f1ba01585b455
tanguybranellec <tanguy.branellec@etu.univ-nantes.fr>
2023-10-14 10:46:03 +0200

    Fonctionnal BOT Maj Back

--- demokratia-tool.go
+++ demokratia-tool.go
@@ -5,77 +5,39 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-var (
-	idOwner  = "675016635869954075"
-	idAdmin  = "1162432736091455600"
-	token    = "---"
-)
-
 func main() {
-	dg, err := discordgo.New("Bot " + token)
-	if err != nil {
-		fmt.Println("Erreur lors de la création de la session Discord:", err)
-		return
-	}
 
-	dg.AddHandler(messageCreate) // Modifiez cette ligne
+	BotToken  := "MTE2MjQzMzQ4ODE0MjczMzM3Mw.GomDvH.4rUW3hAI6cumcn-FaLYgRzhV4nRB6GQvGLhfLA"
+	//AdminRole := "1162432736091455600"
+	OwnerID   := "675016635869954075"
+	//GuildID	  := "1159147284613824587"
+	//Intents := discordgo.Intents(3276799)
+
+
+	dg, err := discordgo.New("Bot " + BotToken)
+	if err != nil {
+		fmt.Println("erreur : type 1")
+	}
+	defer dg.Close()
+
+	dg.Identify.Intents = discordgo.IntentsAll
+
+	dg.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		// Vérifiez si le message commence par "+admin add"
+		if m.Content == "+admin add" && m.Author.ID == OwnerID{
+			s.ChannelMessageSend(m.ChannelID, "Fonctionnel.")
+		}
+	})
 
 	err = dg.Open()
 	if err != nil {
-		fmt.Println("Erreur lors de l'ouverture de la session Discord:", err)
+		fmt.Println("Erreur lors de l'ouverture de la session DiscordGo:", err)
 		return
 	}
+	fmt.Println("bot started")
 
-	fmt.Println("Bot actif. Appuyez sur CTRL+C pour quitter.")
+	// Bloquez le programme pour qu'il s'exécute indéfiniment (vous pouvez gérer proprement la fermeture de la session lorsque vous le souhaitez).
 	select {}
-}
 
 
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == idOwner {
-		// Vérifiez si le message commence par "+admin add" ou "+admin rm"
-		if len(m.Content) > 11 && m.Content[:10] == "+admin add" {
-			userID := m.Content[11:]
-			addAdminRole(s, m.GuildID, userID)
-		} else if len(m.Content) > 10 && m.Content[:9] == "+admin rm" {
-			userID := m.Content[10:]
-			removeAdminRole(s, m.GuildID, userID)
-		}
-	}
-}
-
-func addAdminRole(s *discordgo.Session, guildID, userID string) {
-	// Obtenez le rôle d'admin par ID
-	role, err := s.State.Role(guildID, idAdmin)
-	if err != nil {
-		fmt.Println("Erreur lors de la recherche du rôle d'admin:", err)
-		return
-	}
-
-	// Ajoutez le rôle à l'utilisateur
-	err = s.GuildMemberRoleAdd(guildID, userID, role.ID)
-	if err != nil {
-		fmt.Println("Erreur lors de l'ajout du rôle à l'utilisateur:", err)
-		return
-	}
-
-	fmt.Println("Le rôle", role.Name, "a été ajouté à l'utilisateur", userID)
-}
-
-func removeAdminRole(s *discordgo.Session, guildID, userID string) {
-	// Obtenez le rôle d'admin par ID
-	role, err := s.State.Role(guildID, idAdmin)
-	if err != nil {
-		fmt.Println("Erreur lors de la recherche du rôle d'admin:", err)
-		return
-	}
-
-	// Retirez le rôle de l'utilisateur
-	err = s.GuildMemberRoleRemove(guildID, userID, role.ID)
-	if err != nil {
-		fmt.Println("Erreur lors de la suppression du rôle de l'utilisateur:", err)
-		return
-	}
-
-	fmt.Println("Le rôle", role.Name, "a été enlevé de l'utilisateur", userID)
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,30 @@ module demokratia
 go 1.21.2
 
 require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/andersfylling/disgord v0.36.2 // indirect
+	github.com/andersfylling/snowflake/v5 v5.0.1 // indirect
 	github.com/bwmarrin/discordgo v0.27.1 // indirect
+	github.com/clbanning/mxj v1.8.5-0.20200714211355-ff02cfb8ea28 // indirect
+	github.com/fatih/color v1.12.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/gogf/gf v1.16.9 // indirect
+	github.com/gomodule/redigo v1.8.5 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
-	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
-	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	github.com/grokify/html-strip-tags-go v0.0.1 // indirect
+	github.com/klauspost/compress v1.15.11 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	go.opentelemetry.io/otel v1.0.0 // indirect
+	go.opentelemetry.io/otel/trace v1.0.0 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	golang.org/x/crypto v0.1.0 // indirect
+	golang.org/x/net v0.1.0 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
 )
