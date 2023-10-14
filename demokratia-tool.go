d04fecd500f21454be44261d8da72e23108c5f5c
tanguybranellec <tanguy.branellec@etu.univ-nantes.fr>
2023-10-14 10:53:02 +0200

    Function Commands BETA (!Crashs)

--- demokratia-tool.go
+++ demokratia-tool.go
@@ -2,30 +2,61 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
-
-	BotToken  := "MTE2MjQzMzQ4ODE0MjczMzM3Mw.GomDvH.4rUW3hAI6cumcn-FaLYgRzhV4nRB6GQvGLhfLA"
-	//AdminRole := "1162432736091455600"
-	OwnerID   := "675016635869954075"
-	//GuildID	  := "1159147284613824587"
-	//Intents := discordgo.Intents(3276799)
-
+	BotToken := "---" // Remplacez par le jeton de votre bot
+	OwnerID := "675016635869954075"   // Remplacez par l'ID de l'administrateur/bot owner
+	AdminID := "1162432736091455600" // Remplacez par l'ID du rôle d'administrateur
 
 	dg, err := discordgo.New("Bot " + BotToken)
 	if err != nil {
-		fmt.Println("erreur : type 1")
+		fmt.Println("Erreur lors de la création de la session DiscordGo:", err)
+		return
 	}
 	defer dg.Close()
 
 	dg.Identify.Intents = discordgo.IntentsAll
 
 	dg.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
-		// Vérifiez si le message commence par "+admin add"
-		if m.Content == "+admin add" && m.Author.ID == OwnerID{
-			s.ChannelMessageSend(m.ChannelID, "Fonctionnel.")
+		if m.Author.ID == OwnerID {
+			// Vérifiez si le message commence par "+admin add"
+			if strings.HasPrefix(m.Content, "+admin add ") {
+				// Extrait l'ID de l'utilisateur mentionné dans le message
+				mention := m.Mentions[0]
+				id_user := mention.ID
+
+				// Ajoute le rôle d'administrateur à l'utilisateur
+				err := s.GuildMemberRoleAdd(m.GuildID, id_user, AdminID)
+				if err != nil {
+					fmt.Println("Erreur lors de l'ajout du rôle :", err)
+					s.ChannelMessageSend(m.ChannelID, "Erreur lors de l'ajout du rôle.")
+				} else {
+					s.ChannelMessageSend(m.ChannelID, "Rôle d'administrateur ajouté à l'utilisateur.")
+				}
+			}
+		}
+	})
+
+	dg.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		if m.Author.ID == OwnerID {
+			// Vérifiez si le message commence par "+admin add"
+			if strings.HasPrefix(m.Content, "+admin rm ") {
+				// Extrait l'ID de l'utilisateur mentionné dans le message
+				mention := m.Mentions[0]
+				id_user := mention.ID
+
+				// Ajoute le rôle d'administrateur à l'utilisateur
+				err := s.GuildMemberRoleRemove(m.GuildID, id_user, AdminID)
+				if err != nil {
+					fmt.Println("Erreur lors de l'ajout du rôle :", err)
+					s.ChannelMessageSend(m.ChannelID, "Erreur lors de l'ajout du rôle.")
+				} else {
+					s.ChannelMessageSend(m.ChannelID, "Rôle d'administrateur retiré à l'utilisateur.")
+				}
+			}
 		}
 	})
 
@@ -34,10 +65,7 @@ func main() {
 		fmt.Println("Erreur lors de l'ouverture de la session DiscordGo:", err)
 		return
 	}
-	fmt.Println("bot started")
+	fmt.Println("Bot démarré.")
 
-	// Bloquez le programme pour qu'il s'exécute indéfiniment (vous pouvez gérer proprement la fermeture de la session lorsque vous le souhaitez).
 	select {}
-
-
 }
