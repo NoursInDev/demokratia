eff9aab61d9d855feb422d8d78fcca41858a4752
tanguybranellec <tanguy.branellec@etu.univ-nantes.fr>
2023-10-15 16:26:23 +0200

    Comments

--- demokratia-tool.go
+++ demokratia-tool.go
@@ -8,9 +8,10 @@ import (
 )
 
 func main() {
-	BotToken := "---" // Remplacez par le jeton de votre bot
-	OwnerID := "675016635869954075"   // Remplacez par l'ID de l'administrateur/bot owner
-	AdminID := "1162432736091455600" // Remplacez par l'ID du rôle d'administrateur
+	BotToken := "MTE2MjQzMzQ4ODE0MjczMzM3Mw.GJIaj3.1t6Nn5xiJ1IObGU-08QWso6erMZlzRUPBf2yjg" // Token Bot
+	OwnerID := "675016635869954075"   // Owner ID
+	AdminID := "1162432736091455600" // Admin role ID
+	//MemberID := "1163118801915760720"
 
 	dg, err := discordgo.New("Bot " + BotToken)
 	if err != nil {
@@ -23,16 +24,16 @@ func main() {
 
 	dg.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
 		if m.Author.ID == OwnerID {
-			// Vérifiez si le message commence par "+admin add"
+			// +admin add check
 			if strings.HasPrefix(m.Content, "+admin add ") {
 				if strings.HasPrefix(m.Content, "+admin add <@") && strings.HasSuffix(m.Content, ">") {
 					if len(m.Mentions) == 0 {
 						s.ChannelMessageSend(m.ChannelID, "Aucune mention d'utilisateur trouvée dans le message.")
 						return
 					}
-					// Extrait l'ID de l'utilisateur mentionné dans le message
+					// ID extraction
 					id_user := m.Mentions[0].ID
-					// Ajoute le rôle d'administrateur à l'utilisateur
+					// Role adding
 					err := s.GuildMemberRoleAdd(m.GuildID, id_user, AdminID)
 					if err != nil {
 						fmt.Println("Erreur lors de l'ajout du rôle :", err)
@@ -49,16 +50,16 @@ func main() {
 
 	dg.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
 		if m.Author.ID == OwnerID {
-			// Vérifiez si le message commence par "+admin add"
+			// +admin rm/remove check
 			if strings.HasPrefix(m.Content, "+admin rm ") || strings.HasPrefix(m.Content, "+admin remove ") {
 				if (strings.HasPrefix(m.Content, "+admin rm <@") || strings.HasPrefix(m.Content, "+admin remove <@")) && strings.HasSuffix(m.Content, ">") {
 					if len(m.Mentions) == 0 {
                     s.ChannelMessageSend(m.ChannelID, "Aucune mention d'utilisateur trouvée dans le message.")
                     return
                 }
-					// Extrait l'ID de l'utilisateur mentionné dans le message
+					// ID extraction
 					id_user := m.Mentions[0].ID
-					// Retire le rôle d'administrateur à l'utilisateur
+					// Role removing
 					err := s.GuildMemberRoleRemove(m.GuildID, id_user, AdminID)
 					if err != nil {
 						fmt.Println("Erreur lors de la suppression du rôle :", err)
@@ -75,24 +76,24 @@ func main() {
 
 	dg.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate){
 		if m.Author.ID == OwnerID {
-			// Vérifie si le message commence par "+clear"
+			// +clear check
 			if strings.HasPrefix(m.Content, "+clear") {
-				// Divise le message en mots
+				// Message division
 				parts := strings.Fields(m.Content)
 	
-				// Vérifie qu'il y a au moins deux parties dans le message
+				// Message Conformity check - 2 sides message
 				if len(parts) >= 2 {
-					// Essaie de convertir la deuxième partie en un nombre
+					// 2nd part int convertion
 					numMessages, err := strconv.Atoi(parts[1])
 					if err == nil && numMessages > 0 {
-						// Récupère les messages dans le canal
+						// Tacking back messages
 						messages, getErr := s.ChannelMessages(m.ChannelID, numMessages, "", "", "")
 						if getErr != nil {
 							fmt.Printf("Erreur lors de la récupération des messages: %v\n", getErr)
 							return
 						}
 	
-						// Supprime les messages
+						// messages deleting
 						for _, message := range messages {
 							delErr := s.ChannelMessageDelete(m.ChannelID, message.ID)
 							if delErr != nil {
